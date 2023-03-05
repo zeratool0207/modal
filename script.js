dbb550db2a3ac12f5c3d252905e70fe4266270fc
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-03-06 06:16:01 +0900

    Modal proceeding....

--- script.js
+++ script.js
@@ -5,8 +5,28 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnOpenModal);
+const openModal = function() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
 
 for ( let i = 0; i < btnOpenModal.length; i++ ) {
-    console.log(btnOpenModal[i].textContent);
+    btnOpenModal[i].addEventListener( 'click',openModal );
 }
+
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener( 'click',closeModal );
+// btnCloseModal.addEventListener('click',function () {
+//     modal.classList.add('hidden');
+//     overlay.classList.add('hidden');
+// });
+    
+overlay.addEventListener( 'click',closeModal );
+// overlay.addEventListener('click',function () {
+//     modal.classList.add('hidden');
+//     overlay.classList.add('hidden');
+// });
